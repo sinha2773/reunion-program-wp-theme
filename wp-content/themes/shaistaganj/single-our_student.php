939d7d3736bc7eaf20ee5b92b2fbd60f4a6c2b7f
dolarst <dolarcse123@gmail.com>
2017-12-11 11:30:04 +0600

    muktijoddha studetn single.php updated

--- wp-content/themes/shaistaganj/single-our_student.php
+++ wp-content/themes/shaistaganj/single-our_student.php
@@ -10,6 +10,10 @@
 	                		<div class="all_page_content_title">
 	                			<h2><?php the_title(); ?></h2>
 	                		</div>
+	                		<?php 
+	                			$terms = wp_get_post_terms(get_the_ID(), 'student_type', array("fields" => "all"));
+								$diff_slag = $terms[0]->slug;
+	                		 ?>
 	                	</div>
 	                    <div class="col-md-12">
 	                        <div class="all_page_content_information single_page_content">
@@ -17,8 +21,14 @@
 	                        		<?php the_post_thumbnail(); ?>
 	                        	</div>
 	                        	<div class="single_page_content_image_text">
+	                        	                        		
+	                        		<?php if ($diff_slag == 'schooler_muktijoddha_students') : ?>
+	                        			<h4><?php the_title(); ?></h4>
+		                               	<p>পরিচয় :<?php echo get_post_meta(get_the_ID(),'porichoy',true) ?></p>
+                    					<p>ব্যাচ : <?php echo get_post_meta(get_the_ID(),'batch',true) ?></p>
+	                        			
+	                        		<?php endif; ?>
 	                        		<?php the_content(); ?>
-	                        		
 	                        	</div>
 	                            
 	                        </div>
